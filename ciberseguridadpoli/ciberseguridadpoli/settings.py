e0402ae8f82986f33b66cea269a065f47b84e5bf
Sebastián <spalacioalzate@gmail.com>
2025-07-25 14:51:54 -0500

    JSON de datos

--- ciberseguridadpoli/ciberseguridadpoli/settings.py
+++ ciberseguridadpoli/ciberseguridadpoli/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,6 +88,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse("postgresql://postgres_ciberseguridadpoli_render_user:sf6je7LPDPLMfytQ2lTRnInai7oFtsIj@dpg-d21tb9be5dus73932ceg-a.oregon-postgres.render.com/postgres_ciberseguridadpoli_render")
 
 
 # Password validation
--- ciberseguridadpoli/quiz/urls.py
+++ ciberseguridadpoli/quiz/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 #from rest_framework.documentation import include_docs_urls
 #from rest_framework import routers
 
-from .views import FetchQuizAPIView,Lunerview,QuizView, AvailableQuizzesView, QuizCompletionView,QuizHistoryView
+from .views import FetchQuizAPIView, AvailableQuizzesView, QuizCompletionView,QuizHistoryView
 
 #router = routers.DefaultRouter()
 #router.register(r'quiz',QuizView,'quiz')
@@ -14,7 +14,6 @@ urlpatterns = [
   path("<int:id>/",FetchQuizAPIView.as_view()),
   path("completion/<int:id>/",QuizCompletionView.as_view()),
   path("history",QuizHistoryView.as_view()),
-  path("prueba",Lunerview,name="prueba"),
 
   #path("docs/", include_docs_urls(title="Quiz API"))
 ]
