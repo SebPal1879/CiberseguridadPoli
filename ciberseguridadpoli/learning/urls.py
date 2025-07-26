49df7a15c051f71d6b9a40be7323fa183d08c122
Sebastián <spalacioalzate@gmail.com>
2025-07-26 12:15:29 -0500

    Corregido el nombre del urlpattern add/sections/

--- ciberseguridadpoli/learning/urls.py
+++ ciberseguridadpoli/learning/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
   path('',SectionsView.as_view()),
   path('section/<int:section_id>/',LecturesView.as_view()),
   path('section/<int:section_id>/lecture/<int:lecture_id>/',LectureContentView.as_view()),
-  path('add/learning/',AddSections.as_view()),
+  path('add/sections/',AddSections.as_view()),
   path('add/lectures/',AddLectures.as_view()),
   path('add/contents/',AddLectures.as_view()),
 ]
