6b3bc06ff43c1f1ae90486d721f3063fd9186299
Sebastián <spalacioalzate@gmail.com>
2025-07-28 09:56:48 -0500

    Agregadas las vistas del anterior commit a urls

--- ciberseguridadpoli/quiz/urls.py
+++ ciberseguridadpoli/quiz/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 #from rest_framework.documentation import include_docs_urls
 #from rest_framework import routers
 
-from .views import FetchQuizAPIView, AvailableQuizzesView, QuizCompletionView,QuizHistoryView
+from .views import FetchQuizAPIView, AvailableQuizzesView, QuizCompletionView,QuizHistoryView, AddQuizView, AddQuizAnswerView
 
 #router = routers.DefaultRouter()
 #router.register(r'quiz',QuizView,'quiz')
@@ -14,6 +14,7 @@ urlpatterns = [
   path("<int:id>/",FetchQuizAPIView.as_view()),
   path("completion/<int:id>/",QuizCompletionView.as_view()),
   path("history",QuizHistoryView.as_view()),
-  path("add",)
+  path("add/quiz/",AddQuizView.as_view()),
+  path("add/answer",AddQuizAnswerView.as_view()),
   #path("docs/", include_docs_urls(title="Quiz API"))
 ]
